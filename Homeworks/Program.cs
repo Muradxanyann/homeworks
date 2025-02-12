97ef4c4db5844c35dc32c9411351e67eea67ae32
Gor Muradkhanyan <gormuradxanyan@gmail.com>
2025-02-12 20:01:29 +0400

    some commit

--- Homeworks/Program.cs
+++ Homeworks/Program.cs
@@ -26,7 +26,7 @@ class Program
         }
     }*/
         //Task 13: Convert Days to Weeks and Days
-        string input = Console.ReadLine();
+        /*string input = Console.ReadLine();
         int number;
         int weak = 0, day = 0;
         if (int.TryParse(input, out number))
@@ -34,6 +34,15 @@ class Program
             weak = number / 7;
             day = number % 7;
             Console.WriteLine(($"{number} day is equal {weak} weak and {day} day "));
-        }
+        }*/
+        // Task 14:  Find the Type of Triangle
+        int side1 = Convert.ToInt32(Console.ReadLine());
+        int side2 = Convert.ToInt32(Console.ReadLine());
+        int side3 = Convert.ToInt32(Console.ReadLine());
+        if (side1 == side2 && side1 == side3)
+            Console.WriteLine("This triangle is Equilateral");
+        else if ((side1 == side2 && side1 != side3) || (side2 == side3 && side2 != side1)) 
+            Console.WriteLine("This triangle is Isosceles ");
+        else Console.WriteLine("This triangle is Scalene");
     }
 }
