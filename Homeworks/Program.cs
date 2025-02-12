52590af009d4e9338df402ac354f9e554e918466
Gor Muradkhanyan <gormuradxanyan@gmail.com>
2025-02-12 21:50:20 +0400

    final one

--- Homeworks/Program.cs
+++ Homeworks/Program.cs
@@ -5,7 +5,6 @@ class Program
     static void Main()
     {
         //Task 11: Check If a Number Is Positive, Negative, or Zero 
-        /*
         string input = Console.ReadLine();
         int number;
         if (int.TryParse(input, out number))
@@ -13,10 +12,9 @@ class Program
             if (number > 0) Console.Write($"The number {number} is positive");
             else if (number < 0) Console.Write($"The number {number} is negative");
             else Console.Write($"The number {number} is non-positve && non-negative, its equal to zero");
-        }*/
+        }
         /*------------------------------------------------------------*/
         //Task 12: Check Leap Year
-        /*
         string input = Console.ReadLine();
         int year;
         if (int.TryParse(input, out year))
@@ -25,10 +23,10 @@ class Program
                 Console.Write($"The year {year} is a leep");
             else Console.Write($"The year {year} is not a leep");
         }
-    }*/
+    
         /*------------------------------------------------------------*/
         //Task 13: Convert Days to Weeks and Days
-        /*string input = Console.ReadLine();
+        string input = Console.ReadLine();
         int number;
         int weak = 0, day = 0;
         if (int.TryParse(input, out number))
@@ -36,35 +34,35 @@ class Program
             weak = number / 7;
             day = number % 7;
             Console.WriteLine(($"{number} day is equal {weak} weak and {day} day "));
-        }*/
+        }
         /*------------------------------------------------------------*/
         // Task 14:  Find the Type of Triangle
-        /*int side1 = Convert.ToInt32(Console.ReadLine());
+        int side1 = Convert.ToInt32(Console.ReadLine());
         int side2 = Convert.ToInt32(Console.ReadLine());
         int side3 = Convert.ToInt32(Console.ReadLine());
         if (side1 == side2 && side1 == side3)
             Console.WriteLine("This triangle is Equilateral");
         else if ((side1 == side2 && side1 != side3) || (side2 == side3 && side2 != side1))
             Console.WriteLine("This triangle is Isosceles ");
-        else Console.WriteLine("This triangle is Scalene");*/
+        else Console.WriteLine("This triangle is Scalene");
         /*------------------------------------------------------------*/
         //TASK 15: Check Uppercase or Lowercase
-        /*char input = Convert.ToChar(Console.ReadKey());
+        char input = Convert.ToChar(Console.ReadKey());
         if (char.IsUpper(input))
             Console.WriteLine("Inputed char is upper case");
-        else Console.WriteLine("Inputed char is lower case");*/
+        else Console.WriteLine("Inputed char is lower case");
         /*------------------------------------------------------------*/
         // TASK 16: Find BMI (Body Mass Index)
-        /*int weight = Convert.ToInt32(Console.ReadLine());
+        int weight = Convert.ToInt32(Console.ReadLine());
         double height = Convert.ToDouble(Console.ReadLine());
         double bmi = Convert.ToDouble(weight / (height * height));
         if (bmi < 18.5) Console.Write($"Your BMI is {bmi} and you have Underweight");
         else if (bmi >= 18.5 && bmi <= 24.9) Console.Write($"Your BMI is {bmi} and you have Normal waight");
         else if (bmi >= 25 && bmi <= 29.9) Console.Write($"Your BMI is {bmi} an you have Overweight");
-        else Console.Write("You have Obease");*/
+        else Console.Write("You have Obease");
         /*------------------------------------------------------------*/
         // Task 17: Set or Clear the K-th Bit of a Number
-        /*Console.WriteLine("please input the number and index");
+        Console.WriteLine("please input the number and index");
         int num = Convert.ToInt32(Console.ReadLine());
         int index = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Choose the operation 1 - for set bit, 2 - for clear");
@@ -73,16 +71,16 @@ class Program
             num |= (1 << index);
         else if (operation == 2)
             num &= ~(1 << index);
-        Console.WriteLine(num);*/
+        Console.WriteLine(num);
         /*------------------------------------------------------------*/
         // Task 18: Check if a String Starts or Ends With a Specific Character
-        /*string input = Console.ReadLine();
+        string input = Console.ReadLine();
         char sym = Console.ReadKey().KeyChar;
         char[] arr = input.ToCharArray();
         Console.WriteLine();
         if (arr[0] == sym && arr[^1] == sym)
             Console.WriteLine($"the input is starts and ends with {sym} ");
-        else Console.WriteLine($"the input is not starts and ends with {sym} ");*/
+        else Console.WriteLine($"the input is not starts and ends with {sym} ");
         /*------------------------------------------------------------*/
         //Task 19: Create a 2D Array and Fill It with Numbers
         int[,] array = new int[3, 3];
