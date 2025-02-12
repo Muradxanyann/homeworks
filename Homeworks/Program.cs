1002b48b1745270c2d06fb8c63fb09d0fe1389c5
Gor Muradkhanyan <gormuradxanyan@gmail.com>
2025-02-12 21:46:02 +0400

    final commit

--- Homeworks/Program.cs
+++ Homeworks/Program.cs
@@ -14,6 +14,7 @@ class Program
             else if (number < 0) Console.Write($"The number {number} is negative");
             else Console.Write($"The number {number} is non-positve && non-negative, its equal to zero");
         }*/
+        /*------------------------------------------------------------*/
         //Task 12: Check Leap Year
         /*
         string input = Console.ReadLine();
@@ -25,6 +26,7 @@ class Program
             else Console.Write($"The year {year} is not a leep");
         }
     }*/
+        /*------------------------------------------------------------*/
         //Task 13: Convert Days to Weeks and Days
         /*string input = Console.ReadLine();
         int number;
@@ -35,20 +37,23 @@ class Program
             day = number % 7;
             Console.WriteLine(($"{number} day is equal {weak} weak and {day} day "));
         }*/
+        /*------------------------------------------------------------*/
         // Task 14:  Find the Type of Triangle
         /*int side1 = Convert.ToInt32(Console.ReadLine());
         int side2 = Convert.ToInt32(Console.ReadLine());
         int side3 = Convert.ToInt32(Console.ReadLine());
         if (side1 == side2 && side1 == side3)
             Console.WriteLine("This triangle is Equilateral");
-        else if ((side1 == side2 && side1 != side3) || (side2 == side3 && side2 != side1)) 
+        else if ((side1 == side2 && side1 != side3) || (side2 == side3 && side2 != side1))
             Console.WriteLine("This triangle is Isosceles ");
         else Console.WriteLine("This triangle is Scalene");*/
+        /*------------------------------------------------------------*/
         //TASK 15: Check Uppercase or Lowercase
         /*char input = Convert.ToChar(Console.ReadKey());
-        if (char.IsUpper(input)) 
+        if (char.IsUpper(input))
             Console.WriteLine("Inputed char is upper case");
         else Console.WriteLine("Inputed char is lower case");*/
+        /*------------------------------------------------------------*/
         // TASK 16: Find BMI (Body Mass Index)
         /*int weight = Convert.ToInt32(Console.ReadLine());
         double height = Convert.ToDouble(Console.ReadLine());
@@ -57,7 +62,72 @@ class Program
         else if (bmi >= 18.5 && bmi <= 24.9) Console.Write($"Your BMI is {bmi} and you have Normal waight");
         else if (bmi >= 25 && bmi <= 29.9) Console.Write($"Your BMI is {bmi} an you have Overweight");
         else Console.Write("You have Obease");*/
+        /*------------------------------------------------------------*/
         // Task 17: Set or Clear the K-th Bit of a Number
+        /*Console.WriteLine("please input the number and index");
+        int num = Convert.ToInt32(Console.ReadLine());
+        int index = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Choose the operation 1 - for set bit, 2 - for clear");
+        int operation = Convert.ToInt32(Console.ReadLine());
+        if (operation == 1)
+            num |= (1 << index);
+        else if (operation == 2)
+            num &= ~(1 << index);
+        Console.WriteLine(num);*/
+        /*------------------------------------------------------------*/
+        // Task 18: Check if a String Starts or Ends With a Specific Character
+        /*string input = Console.ReadLine();
+        char sym = Console.ReadKey().KeyChar;
+        char[] arr = input.ToCharArray();
+        Console.WriteLine();
+        if (arr[0] == sym && arr[^1] == sym)
+            Console.WriteLine($"the input is starts and ends with {sym} ");
+        else Console.WriteLine($"the input is not starts and ends with {sym} ");*/
+        /*------------------------------------------------------------*/
+        //Task 19: Create a 2D Array and Fill It with Numbers
+        /*int[,] array = new int[3, 3];
+        Random rand = new Random();
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = rand.Next(100);
+            }
+        }
 
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j] + "\t");
+            }
+
+            Console.WriteLine();
+        }*/
+        /*------------------------------------------------------------*/
+        //Task 20: Create a Jagged Array and Fill It with Numbers 
+        int[][] array = new int[3][];
+        Random rand = new Random();
+        int arrLength = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine($"Input length for array {i + 1}");
+            arrLength = Convert.ToInt32(Console.ReadLine());
+            array[i] = new int[arrLength];
+            for (int j = 0; j < array[i].Length; j++)
+            {
+                array[i][j] = rand.Next(100);
+            }
+        }
+        for (int i = 0; i < array.Length; i++)
+        {
+            for (int j = 0; j < array[i].Length; j++)
+            {
+                Console.Write(array[i][j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+       
     }
 }
--- Homeworks/obj/Debug/net9.0/Homeworks.AssemblyInfo.cs
+++ Homeworks/obj/Debug/net9.0/Homeworks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Homeworks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97ef4c4db5844c35dc32c9411351e67eea67ae32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+471004886e2c3e2044e9cdbc0ef448e676d622f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Homeworks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Homeworks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
