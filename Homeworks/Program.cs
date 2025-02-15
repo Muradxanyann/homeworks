a3dd64a9180c9a235f4212b9376b6411447adccc
Gor Muradkhanyan <gormuradxanyan@gmail.com>
2025-02-16 02:55:34 +0400

    up 3

--- Homeworks/Program.cs
+++ Homeworks/Program.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Reflection.Metadata;
 
-/*
-     Create a Contact class with:
+     /*Create a Contact class with:
     Name, PhoneNumber, and Email properties.
     A method DisplayInfo() to print contact details.
     A Main() method where the user can create 3 contacts and display them.
     Store contacts in an array and allow searching by name.
-    */
-public class Contact
+    #1#*/
+/*public class Contact
 {
     public string Name;
     public string PhoneNumber;
@@ -43,19 +42,109 @@ class Program
         }
 
         Console.WriteLine("Input name to search:");
-        string? input = Console.ReadLine();
+        string input = Console.ReadLine();
         foreach (var contact in contactArray)
         {
-            if (contact.Name == input)
+            if (contact.Name.ToLower() == input.ToLower())
             {
                 Console.WriteLine($"The name was found!!!\n{contact.Name}`s phone number is: {contact.PhoneNumber}\n" +
                                   $"And email: {contact.Email}");
+                return;
             }
-            else
-            {
-                Console.WriteLine($"The {input} is not found in your contacts!!!");
-            }
+            
+        Console.WriteLine($"The {input} is not found in your contacts!!!");
+            
         }
     }
-}
-/*--------------------------------------------------------------------------------------------------------*/
+}*/
+/*--------------------------------------------------------------------------------------------------------
+//Task 2: Student Management
+/*Task: Create a Student class with:
+    Name, Age, and Grade properties.
+    A DisplayDetails() method.
+    In Main(), create 5 students, store them in an array, and display their details.*/
+     /*public class Student
+     {
+         private string Name;
+         private int Age;
+         private double GradeProperties;
+
+         public Student(string name, int age, double properties)
+         {
+             this.Name = name;
+             this.Age = age;
+             this.GradeProperties = properties;
+         }
+
+         public void DisplayDetails()
+         {
+             Console.WriteLine($"Name: {Name}\nAge: {Age}\nGrade Proparties: {GradeProperties}");
+         }
+     }
+     class Program{
+         static void Main(string[] args)
+         {
+             Student[] students = new Student[5]
+             {
+                 new Student("Armen", 20, 9.1),
+                 new Student("Sona", 21, 8.5),
+                 new Student("Karine", 22, 7.8),
+                 new Student("Emma", 23, 8.9),
+                 new Student("Levon", 19, 9.5)
+             };
+             int count = 1;
+             foreach (var i in students)
+             {
+                 Console.WriteLine($"Student {count++} Info");
+                 i.DisplayDetails();
+                 Console.WriteLine();
+             }
+         }
+     }*/
+     //Task 3: Bank Account
+     /*Task: Create a BankAccount class with:
+         AccountNumber, HolderName, Balance.
+         Methods Deposit(amount) and Withdraw(amount), ensuring balance never goes negative.
+         In Main(), allow the user to create an account and perform deposits/withdrawals.
+         Prevent withdrawal if insufficient funds and show an error message.*/
+     public class BankAccount
+     {
+         private int AccountNumber;
+         private string HolderName;
+         private double Balance;
+
+         public BankAccount(int accountNumber, string holderName, double balance)
+         {
+             this.AccountNumber = AccountNumber;
+             this.HolderName = holderName;
+             this.Balance = balance;
+         }
+         public void Deposit(double deposit)
+         {
+             this.Balance += deposit;
+             Console.WriteLine($"Credit Aproved!!!\n Your balance: {Balance}$ ");
+         }
+
+         public void Withdraw(double withdraw)
+         {
+             if (this.Balance - withdraw < 0)
+             {
+                 Console.WriteLine($"Operation failed, your balance is not enough, Your balance: {Balance}");
+                 return;
+             }
+
+             this.Balance -= withdraw;
+             Console.WriteLine($"Purchase Aproved!!!\n Your balance: {Balance}$ ");
+             
+         }
+     }
+
+     class Program
+     {
+         static void Main(string[] args)
+         {
+             BankAccount user = new BankAccount(1, "someName", 100);
+             user.Deposit(10);
+             user.Withdraw(120);
+         }
+     }
--- Homeworks/obj/Debug/net9.0/Homeworks.AssemblyInfo.cs
+++ Homeworks/obj/Debug/net9.0/Homeworks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Homeworks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d240cda9a3a44cec0d639c7fc4cdb1d65e1ee253")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c26512810834dba1c9ee21e1c9fd0bd04c06293")]
 [assembly: System.Reflection.AssemblyProductAttribute("Homeworks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Homeworks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
